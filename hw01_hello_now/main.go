6a5ae99a05814f409465b9982d3e0c29c6cb797c
mavel <mavellol@yandex.ru>
2020-03-07 12:09:07 +0400

    add func

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
+
 func main() {
-	// Place your code here
+	layout := "2006-01-02 15:04:05 +0000 UTC"
+
+	now := time.Now()
+	ntpNow, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("current time: %s\n", now.UTC().Format(layout))
+	fmt.Printf("exact time: %s\n", ntpNow.UTC().Format(layout))
 }
